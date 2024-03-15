63fbe01705ec4067d19e37d6d443d344a86e1481
dvargas42 <dvargas.eng@gmail.com>
2024-03-15 17:29:50 -0300

    feat: :tada: initial commit

---
+++ src/test/java/br/com/alura/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.alura.springbootadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
